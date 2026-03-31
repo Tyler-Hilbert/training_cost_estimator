7349184e7d4759e35e8d9b1205309b489dacd901
Tyler Hilbert <TylerJoshuaHilbert@gmail.com>
2026-03-31 16:34:12 -0400

    Alex Net

--- script.js
+++ script.js
@@ -78,6 +78,47 @@ input_shape = (batch_size, input_size)`,
         loss_function: "nn.CrossEntropyLoss()",
         optimizer: "torch.optim.SGD(model.parameters(), lr=1e-3)",
         training_examples: "1000000"
+    },
+    alexnet: {
+        module_code: `class Model(nn.Module):
+    def __init__(self, num_classes=1000):
+        super(Model, self).__init__()
+        self.conv1 = nn.Conv2d(3, 96, kernel_size=11, stride=4, padding=2)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.maxpool1 = nn.MaxPool2d(kernel_size=3, stride=2)
+        self.conv2 = nn.Conv2d(96, 256, kernel_size=5, padding=2)
+        self.relu2 = nn.ReLU(inplace=True)
+        self.maxpool2 = nn.MaxPool2d(kernel_size=3, stride=2)
+        self.conv3 = nn.Conv2d(256, 384, kernel_size=3, padding=1)
+        self.relu3 = nn.ReLU(inplace=True)
+        self.conv4 = nn.Conv2d(384, 384, kernel_size=3, padding=1)
+        self.relu4 = nn.ReLU(inplace=True)
+        self.conv5 = nn.Conv2d(384, 256, kernel_size=3, padding=1)
+        self.relu5 = nn.ReLU(inplace=True)
+        self.maxpool3 = nn.MaxPool2d(kernel_size=3, stride=2)
+        self.fc1 = nn.Linear(256 * 6 * 6, 4096)
+        self.relu6 = nn.ReLU(inplace=True)
+        self.dropout1 = nn.Dropout(p=0.0)
+        self.fc2 = nn.Linear(4096, 4096)
+        self.relu7 = nn.ReLU(inplace=True)
+        self.dropout2 = nn.Dropout(p=0.0)
+        self.fc3 = nn.Linear(4096, num_classes)
+
+    def forward(self, x):
+        x = self.maxpool1(self.relu1(self.conv1(x)))
+        x = self.maxpool2(self.relu2(self.conv2(x)))
+        x = self.relu3(self.conv3(x))
+        x = self.relu4(self.conv4(x))
+        x = self.maxpool3(self.relu5(self.conv5(x)))
+        x = torch.flatten(x, 1)
+        x = self.dropout1(self.relu6(self.fc1(x)))
+        x = self.dropout2(self.relu7(self.fc2(x)))
+        return self.fc3(x)`,
+        shape_code: `batch_size = 128
+input_shape = (batch_size, 3, 224, 224)`,
+        loss_function: "nn.CrossEntropyLoss()",
+        optimizer: "torch.optim.SGD(model.parameters(), lr=1e-3)",
+        training_examples: "1000000"
     }
 };
 
